30b5893bbede25c81d73059a81cb5f067ca3c92a
Jun99uu <igun0423@naver.com>
2023-01-15 20:23:31 +0900

    add : 기본 디스플레이 스타일 추가

--- src/pages/wrapper/DefaultPageWrapper.tsx
+++ src/pages/wrapper/DefaultPageWrapper.tsx
@@ -1,21 +1,11 @@
 //header와 footer가 있는 페이지의 wrapper이다.
-import { Route, Routes } from "react-router-dom";
-import Home from "../Home";
-import Shop from "../Shop";
-import Subscribe from "../Subscribe";
-import GoodsDetail from "../GoodsDetail";
-import Artist from "../Artist";
 
-const DefaultPageWrapper = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/shop" element={<Shop />} />
-      <Route path="/shop/:goodsId" element={<GoodsDetail />} />
-      <Route path="/subscribe" element={<Subscribe />} />
-      <Route path="/subscribe/:artistId" element={<Artist />} />
-    </Routes>
-  );
+interface pageProps {
+  children: React.ReactNode;
+}
+
+const DefaultPageWrapper = (props: pageProps) => {
+  return <>{props.children}</>;
 };
 
 export default DefaultPageWrapper;
--- src/pages/wrapper/DetailPageWrapper.tsx
+++ src/pages/wrapper/DetailPageWrapper.tsx
@@ -1,19 +1,11 @@
 //header와 footer가 없는 (회원가입 / 장바구니 / 상품구매) 등의 페이지에서 사용되는 wrapper이다.
-import { Route, Routes } from "react-router-dom";
-import Login from "../Login";
-import Register from "../Register";
-import Cart from "../Cart";
-import Order from "../Order";
 
-const DetailPageWrapper = () => {
-  return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/Cart" element={<Cart />} />
-      <Route path="/Order" element={<Order />} />
-    </Routes>
-  );
+interface pageProps {
+  children: React.ReactNode;
+}
+
+const DetailPageWrapper = (props: pageProps) => {
+  return <>{props.children}</>;
 };
 
 export default DetailPageWrapper;
