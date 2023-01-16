aa68650ff0281bf834b3caa8180e58ee81a42c7c
Jun99uu <igun0423@naver.com>
2023-01-16 15:35:20 +0900

    add : 기본 페이지 헤더 컴포넌트 연결

--- src/pages/wrapper/DefaultPageWrapper.tsx
+++ src/pages/wrapper/DefaultPageWrapper.tsx
@@ -1,11 +1,18 @@
 //header와 footer가 있는 페이지의 wrapper이다.
+import Header from "../../components/Header/Headert";
+import "./PageStyle.scss";
 
 interface pageProps {
   children: React.ReactNode;
 }
 
 const DefaultPageWrapper = (props: pageProps) => {
-  return <>{props.children}</>;
+  return (
+    <div className="body">
+      <Header />
+      <div className="child">{props.children}</div>
+    </div>
+  );
 };
 
 export default DefaultPageWrapper;
