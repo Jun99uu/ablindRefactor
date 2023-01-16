cbb5419cf39eeb5f7935d0f9bf4d332f2b0af571
Jun99uu <igun0423@naver.com>
2023-01-16 15:44:50 +0900

    add : 헤더 컴포넌트 추가

---
+++ src/components/Header/Headert.tsx
@@ -0,0 +1,41 @@
+//헤더 wrapper 컴포넌트
+import { useRecoilValue } from "recoil";
+import { isAuthState, userInfoState } from "../../states/auth/isAuthState";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+import "./HeaderStyle.scss";
+
+const Header = () => {
+  const isAuth = useRecoilValue(isAuthState);
+  const userInfo = useRecoilValue(userInfoState);
+  const navigate = useNavigate();
+
+  return (
+    <ul>
+      <li>
+        {isAuth ? (
+          <img src={userInfo.profileImg} className="profile-img" />
+        ) : (
+          <></>
+        )}
+      </li>
+      <li className="logo" onClick={() => navigate("/")}>
+        Ablind
+      </li>
+      <li>
+        {isAuth ? (
+          <span className="menu">
+            <FontAwesomeIcon icon={faBars} />
+          </span>
+        ) : (
+          <span className="login" onClick={() => navigate("/login")}>
+            Login
+          </span>
+        )}
+      </li>
+    </ul>
+  );
+};
+
+export default Header;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 //index(홈) 페이지를 감싸는 컴포넌트이다.
 
 const Home = () => {
-  return <div>홈</div>;
+  return <div></div>;
 };
 
 export default Home;
