f236b8fb7f366645cb46b435944931b979ea1042
Jun99uu <igun0423@naver.com>
2023-01-19 18:44:49 +0900

    add : input태그 useInput 커스텀 훅

---
+++ src/hooks/useInput.tsx
@@ -0,0 +1,13 @@
+import { useState, useCallback, ChangeEvent } from "react";
+
+type onChangeType = (e: ChangeEvent<HTMLInputElement>) => void;
+
+const useInput = (initValue = "") => {
+  const [value, setValue] = useState(initValue);
+  const handler = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  }, []);
+  return [value, handler, setValue] as [string, onChangeType, typeof setValue];
+};
+
+export default useInput;
