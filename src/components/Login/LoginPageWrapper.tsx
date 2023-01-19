6fcc2ac798b33d0dfdd7fea8f916bad1286fa021
Jun99uu <igun0423@naver.com>
2023-01-19 18:49:15 +0900

    add : 로그인 데이터 세팅

--- src/components/Login/LoginPageWrapper.tsx
+++ src/components/Login/LoginPageWrapper.tsx
@@ -1,7 +1,11 @@
 import Button from "../Common/Button";
 import "./LoginPageStyle.scss";
+import useInput from "../../hooks/useInput";
 
 const LoginPageWrapper = () => {
+  const [id, setId] = useInput("");
+  const [pwd, setPwd] = useInput("");
+
   return (
     <div className="login-page-wrapper">
       <div className="titles">
@@ -11,15 +15,25 @@ const LoginPageWrapper = () => {
       <ul>
         <li>
           <label htmlFor="id">ID</label>
-          <input type="text" placeholder="ID를 입력해주세요." />
+          <input
+            id="id"
+            type="text"
+            placeholder="ID를 입력해주세요."
+            onChange={(e) => setId(e)}
+          />
         </li>
         <li>
-          <label htmlFor="id">Password</label>
-          <input type="password" placeholder="Password를 입력해주세요." />
+          <label htmlFor="pwd">Password</label>
+          <input
+            id="pwd"
+            type="password"
+            placeholder="Password를 입력해주세요."
+            onChange={(e) => setPwd(e)}
+          />
         </li>
       </ul>
       <div className="btns">
-        <Button title={`로그인`} handler={() => null} />
+        <Button title={`로그인`} handler={() => console.log(id, pwd)} />
         <Button
           title={`가입하고 특별한 예술가 만나보기!`}
           handler={() => null}
