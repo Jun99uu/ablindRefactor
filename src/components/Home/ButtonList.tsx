586c296663f52f9d76f011fa8859955c8bbe3c6c
Jun99uu <igun0423@naver.com>
2023-02-01 15:50:28 +0900

    add : home 버튼리스트 라우팅 연결

--- src/components/Home/ButtonList.tsx
+++ src/components/Home/ButtonList.tsx
@@ -4,25 +4,27 @@ import {
   faCartShopping,
   faPaintBrush,
 } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 // import "./HomeStyle.scss";
 
 const ButtonList = () => {
+  const navigate = useNavigate();
   return (
     <ul className="button-list">
       <li>
-        <button className="first-btn">
+        <button className="first-btn" onClick={() => navigate("/about")}>
           <FontAwesomeIcon icon={faHeadphones} />
         </button>
         <span>About</span>
       </li>
       <li>
-        <button className="second-btn">
+        <button className="second-btn" onClick={() => navigate("/shop")}>
           <FontAwesomeIcon icon={faCartShopping} />
         </button>
         <span>Shop</span>
       </li>
       <li>
-        <button className="third-btn">
+        <button className="third-btn" onClick={() => navigate("/artist")}>
           <FontAwesomeIcon icon={faPaintBrush} />
         </button>
         <span>Artist</span>
