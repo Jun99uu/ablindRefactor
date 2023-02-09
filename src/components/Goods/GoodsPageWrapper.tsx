88d55cee5a83378af8d674c0fda87e99dbc5b3af
Jun99uu <igun0423@naver.com>
2023-02-09 20:18:43 +0900

    add : add goods default form

--- src/components/Goods/GoodsPageWrapper.tsx
+++ src/components/Goods/GoodsPageWrapper.tsx
@@ -1,12 +1,23 @@
 import TitleBox from "./TitleBox/TitleBox";
 import { goodsDetail } from "./../../interfaces/Goods";
+import { useState } from "react";
 import "./GoodsPageStyle.scss";
+import MenuList from "./MenuList";
+
+export enum Menu {
+  Info = "상세 정보",
+  Review = "리뷰",
+  Qna = "Q&A",
+}
 
 const GoodsPageWrapper = (props: { item: goodsDetail }) => {
   const { item } = props;
+  const [selected, setSelected] = useState<Menu>(Menu.Info);
+
   return (
     <div className="goods-page-wrapper">
       <TitleBox artist={item.author} title={item.name} price={item.price} />
+      <MenuList selected={selected} setSelected={setSelected} />
     </div>
   );
 };
--- src/components/Goods/MenuList.tsx
+++ src/components/Goods/MenuList.tsx
@@ -1,5 +1,26 @@
-const MenuList = () => {
-  return <></>;
+import { Menu } from "./GoodsPageWrapper";
+import { Dispatch, SetStateAction, useState } from "react";
+
+interface Props {
+  selected: Menu;
+  setSelected: Dispatch<SetStateAction<Menu>>;
+}
+
+const MenuList = (props: Props) => {
+  const { selected, setSelected } = props;
+  return (
+    <ul className="menu__list">
+      {Object.values(Menu).map((menu, idx) => (
+        <li
+          key={menu}
+          className={selected === menu ? "selected" : "non--selected"}
+          onClick={() => setSelected(menu)}
+        >
+          {menu}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default MenuList;
